6527ab4cb30b37bd036d9d40e383c1ae2a471c21
Nine <alvedin.gasal@gmail.com>
2023-11-12 22:17:55 +0100

    End of section 19

--- client-app/src/app/stores/profileStore.ts
+++ client-app/src/app/stores/profileStore.ts
@@ -63,8 +63,8 @@ export default class ProfileStore {
             store.userStore.setImage(photo.url);
             runInAction(() => {
                 if (this.profile && this.profile.photos) {
-                    this.profile.photos.find(p => p.isMain)!.isMain = false;
-                    this.profile.photos.find(p => p.id === photo.id)!.isMain = true;
+                    this.profile.photos.find(a => a.isMain)!.isMain = false;
+                    this.profile.photos.find(a => a.id === photo.id)!.isMain = true;
                     this.profile.image = photo.url;
                     this.loading = false;
                 }
--- client-app/src/features/activities/Details/ActivityDetailedChat.tsx
+++ client-app/src/features/activities/Details/ActivityDetailedChat.tsx
@@ -1,8 +1,28 @@
 import { observer } from 'mobx-react-lite'
-import React from 'react'
-import {Segment, Header, Comment, Form, Button} from 'semantic-ui-react'
+import React, { useEffect } from 'react'
+import {Segment, Header, Comment, Loader} from 'semantic-ui-react'
+import { useStore } from '../../../app/stores/store';
+import { Link } from 'react-router-dom';
+import { Formik, Form, Field, FieldProps } from 'formik';
+import * as Yup from 'yup';
+import { formatDistanceToNow } from 'date-fns';
 
-export default observer(function ActivityDetailedChat() {
+interface Props {
+    activityId: string;
+}
+
+export default observer(function ActivityDetailedChat({activityId}: Props) {
+    const {commentStore} = useStore();
+
+    useEffect(() => {
+        if (activityId) {
+            commentStore.createHubConnection(activityId);
+        }
+        return () => {
+            commentStore.clearComments();
+        }
+    }, [commentStore, activityId]);
+
     return (
         <>
             <Segment
@@ -14,45 +34,67 @@ export default observer(function ActivityDetailedChat() {
             >
                 <Header>Chatta om denna händelse</Header>
             </Segment>
-            <Segment attached>
+            <Segment attached clearing>
+            <Formik
+                        onSubmit={(values, {resetForm}) => 
+                            commentStore.addComment(values).then(() => resetForm())}
+                        initialValues={{body: '' }}
+                        validationSchema={Yup.object({
+                            body: Yup.string().required()
+                        })}
+                    >
+                        {({isSubmitting, isValid, handleSubmit }) => (
+                            <Form className='ui form'>
+                                <Field name='body'>
+                                    {(props: FieldProps) => (
+                                        <div style={{position: 'relative'}}>
+                                            <Loader active={isSubmitting} />
+                                            <textarea 
+                                                placeholder='Skriv din kommentar här (Tryck Enter för att kommentera, SHIFT + Enter för att gå till nästa rad)'
+                                                rows={2}
+                                                {...props.field}
+                                                onKeyDown={e => {
+                                                    if (e.key === 'Enter' && e.shiftKey) {
+                                                        return;
+                                                    }
+                                                    if (e.key === 'Enter' && !e.shiftKey) {
+                                                        e.preventDefault();
+                                                        isValid && handleSubmit();
+                                                    }
+                                                }}
+                                            />
+                                        </div>
+                                    )}
+                                </Field>
+                            {/* <MyTextArea placeholder='Skriv ett kommentar' name='body' rows={2} />
+                            <Button
+                                loading={isSubmitting}
+                                disabled={isSubmitting || !isValid}
+                                content='Lägg till ett svar'
+                                labelPosition='left'
+                                icon='edit'
+                                primary
+                                type='submit'
+                                floated='right'
+                            /> */}
+                        </Form>
+                        )}
+                    </Formik>
                 <Comment.Group>
-                    <Comment>
-                        <Comment.Avatar src='/assets/user.png'/>
+                    {commentStore.comments.map(comment => (
+                    <Comment key={comment.id}>
+                        <Comment.Avatar src={comment.image || '/assets/user.png'} />
                         <Comment.Content>
-                            <Comment.Author as='a'>Alvedin</Comment.Author>
+                            <Comment.Author as={Link} to={`/profiles/${comment.username}`}>
+                                   {comment.displayName}
+                            </Comment.Author>
                             <Comment.Metadata>
-                                <div>Idag kl.17:42</div>
+                                <div>{formatDistanceToNow(comment.createdAt)} ago</div>
                             </Comment.Metadata>
-                            <Comment.Text>Vad konstnärligt!</Comment.Text>
-                            <Comment.Actions>
-                                <Comment.Action>Svara</Comment.Action>
-                            </Comment.Actions>
+                            <Comment.Text style={{whiteSpace: 'pre-wrap'}}>{comment.body}</Comment.Text>
                         </Comment.Content>
                     </Comment>
-
-                    <Comment>
-                        <Comment.Avatar src='/assets/user.png'/>
-                        <Comment.Content>
-                            <Comment.Author as='a'>Erik</Comment.Author>
-                            <Comment.Metadata>
-                                <div>5 dagar sedan</div>
-                            </Comment.Metadata>
-                            <Comment.Text>Det här är fantastiskt. Tack så mycket</Comment.Text>
-                            <Comment.Actions>
-                                <Comment.Action>Svara</Comment.Action>
-                            </Comment.Actions>
-                        </Comment.Content>
-                    </Comment>
-
-                    <Form reply>
-                        <Form.TextArea/>
-                        <Button
-                            content='Lägg till ett svar'
-                            labelPosition='left'
-                            icon='edit'
-                            primary
-                        />
-                    </Form>
+                    ))}
                 </Comment.Group>
             </Segment>
         </>
